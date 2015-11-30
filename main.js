759975d3d6e78cdbc5883ac55b84de33641c48bf
Goblortikus <goblortikus-github@yahoo.com>
2015-11-30 09:15:06 -0800

    added cleaner function 2 in comments

--- main.js
+++ main.js
@@ -7,16 +7,41 @@ function smallestDiffWith1000 (a,b) {
 	return (Math.abs(1000-a) < Math.abs(1000-b)) ? a : b;
 }
  
-// function 2 and function 4 are identical. Recursively doubles characters by default, or add optional second tweener character for tweening. Bonus: handles empty string and numbers.
+
+
+// function 2 and function 4 are identical. Recursively doubles characters by default, or add optional second tweener character for tweening. Bonus: handles empty string and most numbers (won't handle non 0 that start with 0).
 function tweeny(s, tween) {
-    return ( (s = '' + s) ?  s[0] + (tween ? (s.length > 1 ? tween : '' ) : s[0]) + tweeny(s.substring(1, s.length), tween) : '');
+    return ( (s = '' + s) && s?  s[0] + (tween ? (s.length > 1 ? tween : '' ) : s[0]) + tweeny(s.substring(1, s.length), tween) : '');
 }
+
+// a more robust version of basic function 2 (cleaner, simpler, handles undefined and still handles empty string and most numbers but does not cover function 4 case)
+/*
+function tweeny(s){
+    return s=(''+s) && s && s.split('').reduce(function(p,c){return p+c+c},'');
+}
+*/
 
 // function 3
 function splitter(arr) {
     return arr.length > 1 && !((this.sum = arr.reduce(function(p,c){return p+c}, 0))%2) && arr.slice(0,-1).some(function(x,i,a){return (this.acc+=x) == sum/2}, {acc: 0});   
 }
-
+// a more readable version of function 3
+// if splittable, there must be a left subarray which equals the remaining right subarray, so at some point the right subarray must equal half the total sum.
+// one simple algorithm adds up the entire list to get a total, then starts over at the end summing until it finds a sum equal to half of the total. Could also start re-summing from beginning.
+/*
+function splitter(arr) {
+    var frontSum = backSum = 0;
+    if (arr.length > 1) {
+        frontSum = arr.reduce(function(p,c) {return p+c});
+        for (var i = arr.length - 1; i > 0; i--) {
+            if ((backSum += arr[i]) == frontSum * .5) {
+                return true;
+            }
+        }
+    }
+    return false;  
+}
+*/
 
 // function 4 - recursive
 // IDENTICAL to function 2 (which is already recursive); just add optional second param to serve as "tween" character.
